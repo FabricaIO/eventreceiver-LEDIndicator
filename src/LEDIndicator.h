c8ec0e1a317e322555a03161b52928e6b3f6baec
Sam Groveman <samgroveman@outlook.com>
2025-10-25 22:47:26 -0400

    Fixed spacing in comments

--- src/LEDIndicator.h
+++ src/LEDIndicator.h
@@ -33,7 +33,7 @@ class LEDIndicator : public EventReceiver {
 		int color_map[8] = {
 		0x000000,     // Off    (0 blinks)
 		0x007F00,     // Green  (1 blinks)
-		0x00B0FF,     // Cyan	(2 blinks)
+		0x00B0FF,     // Cyan   (2 blinks)
 		0xFF6000,     // Yellow (3 blinks)
 		0x0000FF,     // Blue   (4 blinks)
 		0xFF2800,     // Orange (5 blinks)
